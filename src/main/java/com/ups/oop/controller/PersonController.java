27dc4ffc48e71dab71cee3f78a0ed167bc912029
roger_beltran <rbeltranv@est.ups.edu.ec>
2024-08-18 21:52:33 -0500

    Add(I): Edit Import Wildcart

--- src/main/java/com/ups/oop/controller/PersonController.java
+++ src/main/java/com/ups/oop/controller/PersonController.java
@@ -4,8 +4,13 @@ import com.ups.oop.dto.Person;
 import com.ups.oop.service.PersonService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 
